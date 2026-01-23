920432e5462078751660b268af530510afdc253e
mryan1 <mryan1@login4.stampede3.tacc.utexas.edu>
2026-01-23 13:54:40 -0600

    TST: Add randomization to search timing

--- examples/timing_tests.py
+++ examples/timing_tests.py
@@ -3,6 +3,7 @@ import yt
 from scipy.spatial import KDTree
 from yt.units import Msun, kiloparsec
 
+import packingcubes.bounding_box as bbox
 import packingcubes.data_objects as data_objects
 import packingcubes.octree as octree
 import packingcubes.packed_tree as optree
@@ -13,14 +14,20 @@ simname = "IllustrisTNG"
 ill_path = data_path / simname
 snapfile = ill_path / "snapshot_090.hdf5"
 
-center = np.array([15000, 10000, 12500])
-radius = 1000
+rng = np.random.default_rng(0xBA55ADE89)
+
+centers = []
+radii = []
 
 
 def load_data(decimation_factor=10):
     ds = data_objects.GadgetishHDF5Dataset(name=simname, filepath=snapfile)
     ds._positions = ds._positions[: int(len(ds) / decimation_factor), :]
     ds._setup_index()
+    min_bounds = np.min(ds.positions, axis=0)
+    max_bounds = np.max(ds.positions, axis=0)
+    ds._box = bbox.make_bounding_box(np.hstack((min_bounds, max_bounds - min_bounds)))
+    random_search_balls(ds)
     return ds
 
 
@@ -32,6 +39,13 @@ def reset_data(ds):
     return ds
 
 
+def random_search_balls(ds):
+    box = ds.bounding_box
+    for _ in range(100):
+        centers.append(rng.random(box.size.size) * box.size + box.position)
+        radii.append(10 ** (rng.random() * np.log10(rng.choice(box.size))))
+
+
 def python_octree_creation(ds):
     return octree.PythonOctree(
         dataset=ds,
@@ -39,10 +53,11 @@ def python_octree_creation(ds):
 
 
 def python_octree_query_ball_point(tree: octree.Octree):
-    sph_inds = tree.get_particle_indices_in_sphere(
-        center=center,
-        radius=radius,
-    )
+    for c, r in zip(centers, radii, strict=True):
+        sph_inds = tree.get_particle_indices_in_sphere(
+            center=c,
+            radius=r,
+        )
     # sph_nodes_entire,sph_nodes_partial = tree._get_nodes_in_sphere(
     #     center=center, radius=radius
     # )
@@ -54,10 +69,11 @@ def packed_octree_creation(ds):
 
 
 def packed_octree_query_ball_point(tree: optree.PackedTree):
-    sph_inds = tree.get_particle_indices_in_sphere(
-        center=center,
-        radius=radius,
-    )
+    for c, r in zip(centers, radii, strict=True):
+        sph_inds = tree.get_particle_indices_in_sphere(
+            center=c,
+            radius=r,
+        )
 
 
 # we want the PackedTree stuff to be pre-compiled
@@ -70,7 +86,8 @@ def kdtree_creation(ds):
 
 
 def kdtree_query_ball_point(tree: KDTree):
-    sph_inds = tree.query_ball_point(x=center, r=radius)
+    for c, r in zip(centers, radii, strict=True):
+        sph_inds = tree.query_ball_point(x=c, r=r)
 
 
 def yt_setup(decimation_factor=10):
@@ -93,7 +110,7 @@ def yt_setup(decimation_factor=10):
 
 
 def yt_creation(ytdata):
-    return ytdata.sphere((center, "kpc"), (radius, "kpc"))
+    return ytdata.sphere((centers[0], "kpc"), (radii[0], "kpc"))
 
 
 def yt_search(sph):
