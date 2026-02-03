BUG: Tighten valid box precision check
Previous check could allow a really small box in one dimension to slip through,

e.g. box[2]=1e-245 and box[5]=1e-245

packingcubes/bounding_box.py
@@ -128,7 +128,10 @@ def check_valid(box: BoxLike, *, raise_error: bool = True) -> BoundingBoxValidFl
             np.isfinite(box[:3])
             & np.isfinite(box[3:])
             & (box[3:] > 0)
-            & (box[3:] < (np.abs(box[:3]) + box[3:]) * np.finfo(float).eps),
+            & (
+                box[3:]
+                < (np.maximum(np.abs(box[:3]) + box[3:], 1.0)) * np.finfo(float).eps
+            ),
         ):
             flag ^= BoundingBoxValidFlag.PRECISION
     if raise_error and flag != BoundingBoxValidFlag.VALID: