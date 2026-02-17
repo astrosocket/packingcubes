cd4764b7e7108f54f7c64a7840120b2e372db97a
Michael Ryan <mtryan83@gmail.com>
2026-02-17 15:52:57 -0500

    ENH: Add packingcubes level logger explicitly

--- packingcubes/__init__.py
+++ packingcubes/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from packingcubes.data_objects import GadgetishHDF5Dataset as HDF5Dataset
 from packingcubes.packed_tree import PackedTree as Optree
 
@@ -5,3 +7,6 @@ __all__ = [
     "Optree",
     "HDF5Dataset",
 ]
+
+
+logging.getLogger("packingcubes")
