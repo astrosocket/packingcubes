c8d152a2b4db91d06ac5c4e56c2661004fd35e59
Michael Ryan <mtryan83@gmail.com>
2026-03-08 14:11:34 -0400

    ENH: Add version information to main __init__.py

--- packingcubes/__init__.py
+++ packingcubes/__init__.py
@@ -5,11 +5,14 @@ from packingcubes.cubes import make_cubes as make_cubes
 from packingcubes.data_objects import GadgetishHDF5Dataset as HDF5Dataset
 from packingcubes.packed_tree import PackedTree as Optree
 
+from ._version import __version__
+
 __all__ = [
     "Optree",
     "HDF5Dataset",
     "make_cubes",
     "Cubes",
+    "__version__",
 ]
 
 
