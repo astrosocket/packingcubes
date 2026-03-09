e6813be904c0d08cc9273c58438e724d1ad8d232
Michael Ryan <mtryan83@gmail.com>
2026-03-09 17:12:25 -0400

    ENH: Export KDTree

--- packingcubes/__init__.py
+++ packingcubes/__init__.py
@@ -3,6 +3,7 @@ import logging
 from packingcubes.cubes import Cubes as Cubes
 from packingcubes.cubes import make_cubes as make_cubes
 from packingcubes.data_objects import GadgetishHDF5Dataset as HDF5Dataset
+from packingcubes.packed_tree import KDTree as KDTree
 from packingcubes.packed_tree import PackedTree as Optree
 
 from ._version import __version__
@@ -10,6 +11,7 @@ from ._version import __version__
 __all__ = [
     "Optree",
     "HDF5Dataset",
+    "KDTree",
     "make_cubes",
     "Cubes",
     "__version__",
